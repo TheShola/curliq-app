6a6dffa0111260dba48917b60f9b022cea6b7eac
theshola <adesolaabimbola@gmail.com>
2018-07-22 12:36:44 -0400

    Initial Commit - Camera Feature, Gallery Feature

---
+++ App.js
@@ -0,0 +1,262 @@
+import { Constants, Camera, FileSystem, Permissions } from 'expo';
+import React from 'react';
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Slider,
+  Platform
+} from 'react-native';
+import GalleryScreen from './GalleryScreen';
+import isIPhoneX from 'react-native-is-iphonex';
+
+import { 
+  Ionicons,
+  MaterialIcons,
+  Foundation,
+  MaterialCommunityIcons,
+  Octicons
+} from '@expo/vector-icons';
+
+const landmarkSize = 2;
+
+const flashModeOrder = {
+  off: 'on',
+  on: 'auto',
+  auto: 'torch',
+  torch: 'off',
+};
+
+const flashIcons = {
+  off: 'flash-off',
+  on: 'flash-on',
+  auto: 'flash-auto',
+  torch: 'highlight'
+};
+
+
+export default class CameraScreen extends React.Component {
+  state = {
+    flash: 'off',
+    zoom: 0,
+    autoFocus: 'on',
+    type: 'back',
+    newPhotos: false,
+    permissionsGranted: false,
+    showGallery: false,
+    showMoreOptions: false   //uploadPhoto: false
+
+  };
+
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ permissionsGranted: status === 'granted' });
+  }
+
+  componentDidMount() {
+    FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'photos').catch(e => {
+      console.log(e, 'Directory exists');
+    });
+  }
+
+
+  toggleView = () => this.setState({ showGallery: !this.state.showGallery, newPhotos: false });
+
+
+  //this button will change to upload photo
+  toggleMoreOptions = () => this.setState({ showMoreOptions: !this.state.showMoreOptions });
+
+  toggleFacing = () => this.setState({ type: this.state.type === 'back' ? 'front' : 'back' });
+
+  toggleFlash = () => this.setState({ flash: flashModeOrder[this.state.flash] });
+
+  toggleFocus = () => this.setState({ autoFocus: this.state.autoFocus === 'on' ? 'off' : 'on' });
+
+  zoomOut = () => this.setState({ zoom: this.state.zoom - 0.1 < 0 ? 0 : this.state.zoom - 0.1 });
+
+  zoomIn = () => this.setState({ zoom: this.state.zoom + 0.1 > 1 ? 1 : this.state.zoom + 0.1 });
+
+  setFocusDepth = depth => this.setState({ depth });
+
+
+  takePicture = () => {
+    if (this.camera) {
+      this.camera.takePictureAsync({ onPictureSaved: this.onPictureSaved });
+    }
+  };
+
+  onPictureSaved = async photo => {
+    await FileSystem.moveAsync({
+      from: photo.uri,
+      to: `${FileSystem.documentDirectory}photos/${Date.now()}.jpg`,
+    });
+    this.setState({ newPhotos: true });
+  }
+
+  renderGallery() {
+    return <GalleryScreen onPress={this.toggleView.bind(this)} />;
+  }
+
+  renderNoPermissions = () => 
+    <View style={styles.noPermissions}>
+      <Text style={{ color: 'white' }}>
+        Camera permissions not granted - cannot open camera preview.
+      </Text>
+    </View>
+
+  renderTopBar = () => 
+    <View
+      style={styles.topBar}>
+      <TouchableOpacity style={styles.toggleButton} onPress={this.toggleFacing}>
+        <Ionicons name="ios-reverse-camera" size={32} color="white" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.toggleButton} onPress={this.toggleFlash}>
+        <MaterialIcons name={flashIcons[this.state.flash]} size={32} color="white" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.toggleButton} onPress={this.toggleFocus}>
+        <Text style={[styles.autoFocusLabel, { color: this.state.autoFocus === 'on' ? "white" : "#6b6b6b" }]}>AF</Text>
+      </TouchableOpacity>  
+    </View>
+
+  renderBottomBar = () =>
+    <View
+      style={styles.bottomBar}>
+      <TouchableOpacity style={styles.bottomButton} onPress={this.toggleMoreOptions}>
+        <Octicons name="kebab-horizontal" size={30} color="white"/>
+      </TouchableOpacity>
+      <View style={{ flex: 0.4 }}>
+        <TouchableOpacity
+          onPress={this.takePicture}
+          style={{ alignSelf: 'center' }}>
+          <Ionicons name="ios-radio-button-on" size={70} color="white" />
+        </TouchableOpacity>
+      </View> 
+      <TouchableOpacity style={styles.bottomButton} onPress={this.toggleView}>
+        <View>
+          <Foundation name="thumbnails" size={30} color="white" />
+          {this.state.newPhotos && <View style={styles.newPhotosDot}/>}
+        </View>
+      </TouchableOpacity>
+    </View>
+
+  renderMoreOptions = () =>
+    (
+      <View style={styles.options}>
+        
+      </View> 
+    );
+
+  renderCamera = () =>
+    (
+      <View style={{ flex: 1 }}>
+        <Camera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.camera}
+          type={this.state.type}
+          flashMode={this.state.flash}
+          autoFocus={this.state.autoFocus}
+          zoom={this.state.zoom}>
+          {this.renderTopBar()}
+          {this.renderBottomBar()}
+        </Camera>
+        {this.state.showMoreOptions && this.renderMoreOptions()}
+      </View>
+    );
+
+  render() {
+    const cameraScreenContent = this.state.permissionsGranted
+      ? this.renderCamera()
+      : this.renderNoPermissions();
+    const content = this.state.showGallery ? this.renderGallery() : cameraScreenContent;
+    return <View style={styles.container}>{content}</View>;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  camera: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  topBar: {
+    flex: 0.2,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingTop: Constants.statusBarHeight / 2,
+  },
+  bottomBar: {
+    paddingBottom: isIPhoneX ? 25 : 5,
+    backgroundColor: 'transparent',
+    alignSelf: 'flex-end',
+    justifyContent: 'space-between',
+    flex: 0.12,
+    flexDirection: 'row',
+  },
+  noPermissions: {
+    flex: 1,
+    alignItems:'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+  gallery: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  toggleButton: {
+    flex: 0.25,
+    height: 40,
+    marginHorizontal: 2,
+    marginBottom: 10,
+    marginTop: 20,
+    padding: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  autoFocusLabel: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  bottomButton: {
+    flex: 0.3, 
+    height: 58, 
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  newPhotosDot: {
+    position: 'absolute',
+    top: 0,
+    right: -5,
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: '#4630EB'
+  },
+  options: {
+    position: 'absolute',
+    bottom: 80,
+    left: 30,
+    width: 200,
+    height: 160,
+    backgroundColor: '#000000BA',
+    borderRadius: 4,
+    padding: 10,
+  },
+  landmark: {
+    width: landmarkSize,
+    height: landmarkSize,
+    position: 'absolute',
+    backgroundColor: 'red',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+});
---
+++ README.md
@@ -0,0 +1,43 @@
+Curl IQ App
+TODO: 
+		- Allows users to take pictures and allows users to upload photos from their camera roll
+		- Allows users to save photos in an app gallery
+		- Allows users to sign in using email/password, google and facebook authentication
+		- Allows users to view their curl IQ Profile
+		- Allows users to see hair tips based on their location
+		- Allows users to see their hair type
+
+Installation
+	Pre-Installation 
+		- CURL IQ is built on Expo
+		- Expo apps are React Native apps which contain the Expo SDK. The SDK is a native-and-JS library which provides access to the device's system functionality (things like the camera, contacts, local storage, and other hardware). That means you don't need to use Xcode or Android Studio, or write any native code, and it also makes your pure-JS project very portable because it can run in any native environment containing the Expo SDK.
+		
+		- Follow this - https://docs.expo.io/versions/latest/introduction/installation.html
+
+TODO: 
+		- git clone repo
+		- npm install or yarn start 
+		- yarn add react-native
+
+Usage
+TODO: Write usage instructions
+
+Contributing
+Fork it!
+Create your feature branch: git checkout -b my-new-feature //git checkout -b -upload-photos-feature
+Commit your changes: git commit -m 'Add some feature'  //git commit -m 'Add camera roll feature'
+Push to the branch: git push origin my-new-feature
+
+
+
+
+History
+TODO: Write history
+
+Credits
+TODO: Write credits
+
+License
+TODO: Write license
+
+
